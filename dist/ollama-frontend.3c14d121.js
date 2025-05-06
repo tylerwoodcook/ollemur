1a14c1fe2d4021756eec9dc6281e7ae77c19c84e
Tyler <twoodcook@gmail.com>
2025-05-05 20:12:30 -0400

    npm run build

--- dist/ollama-frontend.3c14d121.js
+++ dist/ollama-frontend.3c14d121.js
@@ -162,12 +162,12 @@
   }
 })(
   {
-    n0fw4: [
+    gRcc0: [
       function (require, module, exports, __globalThis) {
         var global = arguments[3];
         var HMR_HOST = null;
         var HMR_PORT = null;
-        var HMR_SERVER_PORT = 1234;
+        var HMR_SERVER_PORT = 57256;
         var HMR_SECURE = false;
         var HMR_ENV_HASH = "439701173a9199ea";
         var HMR_USE_SSE = false;
@@ -3580,7 +3580,7 @@ ${frame.code}`;
       {},
     ],
   },
-  ["n0fw4", "kTBnD"],
+  ["gRcc0", "kTBnD"],
   "kTBnD",
   "parcelRequire8d18",
   {},
